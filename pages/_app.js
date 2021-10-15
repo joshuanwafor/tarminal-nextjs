6f3dbc99d17c97548b95befac02a94c496891709
joshua <joshuanwafor01@gmail.com>
2021-10-15 16:46:13 +0100

    oik

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
 
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <div> 
+    <link href="/css/bootstrap.min.css" rel="stylesheet"/>
+
+<Component {...pageProps} />
+
+<script src="/js/bootstrap.bundle.min.js" ></script>
+</div>
+
 }
 
 export default MyApp
